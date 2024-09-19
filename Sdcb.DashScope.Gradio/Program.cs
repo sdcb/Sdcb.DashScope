899bfddd80b84581856ed213fb2aa61a929a66ab
sdcb <sdflysha@qq.com>
2024-09-19 12:52:40 +0800

    use qwen2.5

--- Sdcb.DashScope.Gradio/Program.cs
+++ Sdcb.DashScope.Gradio/Program.cs
@@ -22,11 +22,18 @@ static Blocks CreateBlocks(IServiceProvider sp)
     Textbox systemPrompt;
 
     gr.Markdown("# 通义千问开源模型");
-    string[] knownModels = 
-    [
-        "qwen2-57b-a14b-instruct", "qwen2-72b-instruct", "qwen2-7b-instruct", "qwen2-1.5b-instruct", "qwen2-0.5b-instruct",
-        "qwen1.5-110b-chat", "qwen1.5-72b-chat", "qwen1.5-32b-chat", "qwen1.5-14b-chat", "qwen1.5-7b-chat", "qwen1.5-1.8b-chat", "qwen1.5-0.5b-chat", "codeqwen1.5-7b-chat"
-    ];
+    string[] knownModels = """
+        qwen2.5-0.5b-instruct
+        qwen2.5-1.5b-instruct
+        qwen2.5-3b-instruct
+        qwen2.5-14b-instruct
+        qwen2.5-32b-instruct
+        qwen2.5-72b-instruct
+        qwen2.5-coder-7b-instruct
+        qwen2.5-math-1.5b-instruct
+        qwen2.5-math-7b-instruct
+        qwen2.5-math-72b-instruct
+        """.Split("\r\n");
     Dropdown model;
 
     using (gr.Row())
